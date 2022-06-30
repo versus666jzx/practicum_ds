492f8fb311d338c84054d542b7878c7a9812742f
ChuckNorris <ChuckNorris@gmail.com>
2022-06-30 18:31:36 +0300

    Continue working

--- 17_sprint_CV_age determination/notebook/task.py
+++ 17_sprint_CV_age determination/notebook/task.py
@@ -1,53 +1,49 @@
-from tensorflow.keras.datasets import fashion_mnist
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, AvgPool2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, AvgPool2D
 import numpy as np
 
 
 def load_train(path):
-    datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        horizontal_flip=True,
-        vertical_flip=True
-    )
-
-    train_datagen = datagen.flow_from_directory(
-        path,
-        batch_size=16,
-        target_size=(150, 150),
-        subset='training',
-        class_mode='sparse',
-        seed=12345)
-    return train_datagen
+	train_datagen = ImageDataGenerator(rescale=1. / 255,
+	                                   horizontal_flip=True,
+	                                   vertical_flip=True)
+	train_datagen_flow = train_datagen.flow_from_directory(
+		path,
+		target_size=(150, 150),
+		batch_size=16,
+		class_mode='sparse',
+		subset='training',
+		seed=12345)
+	return (train_datagen_flow)
 
 
 def create_model(input_shape):
-    optimizer = Adam(lr=0.03)
-    model = Sequential()
-    model.add(Conv2D(6, (5, 5), padding='same', activation='relu',
-                     input_shape=input_shape))
-    model.add(AvgPool2D(pool_size=(2, 2)))
-    model.add(Conv2D(16, (5, 5), padding='same', activation='relu'))
-    model.add(AvgPool2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(units=120, activation='relu'))
-    model.add(Dense(units=12, activation='softmax'))
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
-                  metrics=['acc'])
-    return model
+	model = Sequential()
+	optimizer = Adam(lr=0.005)
+
+	model.add(Conv2D(6, (5, 5), padding='same', activation='relu', input_shape=input_shape))
+	model.add(AvgPool2D(pool_size=(2, 2)))
+	model.add(Conv2D(16, (5, 5), padding='same', activation='relu'))
+	model.add(AvgPool2D(pool_size=(2, 2)))
+	model.add(Flatten())
+	model.add(Dense(units=64, activation='relu'))
+	model.add(Dense(units=32, activation='relu'))
+	model.add(Dense(units=12, activation='softmax'))
+
+	model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['acc'])
+
+	return model
 
 
-def train_model(model, train_data, test_data, epochs=5, batch_size=16,
-                steps_per_epoch=None, validation_steps=None):
-    features_train, target_train = next(train_data)
-    model.fit(features_train, target_train,
-              batch_size=batch_size,
-              validation_data=test_data,
-              epochs=epochs,
-              steps_per_epoch=steps_per_epoch,
-              validation_steps=validation_steps,
-              verbose=2, shuffle=True)
-
-    return model
+def train_model(model, train_data, test_data, batch_size=None,
+                steps_per_epoch=None, validation_steps=None,
+                verbose=2, epochs=20):
+	model.fit(train_data,
+	          validation_data=test_data,
+	          batch_size=batch_size,
+	          steps_per_epoch=steps_per_epoch,
+	          validation_steps=validation_steps,
+	          verbose=verbose, epochs=epochs)
+	return model
