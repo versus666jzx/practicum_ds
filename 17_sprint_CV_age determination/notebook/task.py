2787c5bc918317f6a2b12f68f80f257726170865
ChuckNorris <ChuckNorris@gmail.com>
2022-07-05 18:41:36 +0300

    Continue working

--- 17_sprint_CV_age determination/notebook/task.py
+++ 17_sprint_CV_age determination/notebook/task.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.datasets import fashion_mnist
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, AvgPool2D
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, AvgPool2D, GlobalAveragePooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.models import Sequential
@@ -21,16 +21,18 @@ def load_train(path):
         subset='training',
         class_mode='sparse',
         seed=12345)
+
     return train_datagen
 
 
 def create_model(input_shape):
     backbone = ResNet50(input_shape=input_shape,
-                        weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                        weights='imagenet',
                         include_top=False)
     model = Sequential()
     model.add(backbone)
-    model.add(Dense(units=12))
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(units=12,  activation='softmax'))
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
                   metrics=['acc'])
     return model
@@ -38,6 +40,7 @@ def create_model(input_shape):
 
 def train_model(model, train_data, test_data, epochs=5, batch_size=16,
                 steps_per_epoch=None, validation_steps=None):
+
     model.fit(train_data,
               validation_data=test_data,
               epochs=epochs,
