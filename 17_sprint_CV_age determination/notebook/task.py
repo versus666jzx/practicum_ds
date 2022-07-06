35e90f3ab7955fa6cde8664e926ae550efe1909a
ChuckNorris <ChuckNorris@gmail.com>
2022-07-06 19:19:17 +0300

    Continue working

--- 17_sprint_CV_age determination/notebook/task.py
+++ 17_sprint_CV_age determination/notebook/task.py
@@ -1,75 +1,76 @@
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.layers import Conv2D, Flatten, Dense, AvgPool2D, GlobalAveragePooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-import numpy as np
 
 
 def load_train(path):
+    labels = pd.read_csv(path + 'labels.csv')
     datagen = ImageDataGenerator(
-        rescale=1. / 255,
+        validation_split=0.25,
         horizontal_flip=True,
-        vertical_flip=True
-    )
+        vertical_flip=True,
+        rescale=1. / 255)
 
-    labels = pd.read_csv(f'{path}/labels.csv')
-    train_datagen = datagen.flow_from_dataframe(
-        labels,
-        path,
+    train_gen_flow = datagen.flow_from_dataframe(
+        dataframe=labels,
+        directory=path + 'final_files/',
+        x_col='file_name',
+        y_col='real_age',
+        target_size=(224, 224),
         batch_size=16,
-        target_size=(150, 150),
+        class_mode='raw',
         subset='training',
-        class_mode='sparse',
         seed=12345)
 
-    return train_datagen
+    return train_gen_flow
 
 
 def load_test(path):
+    labels = pd.read_csv(path + 'labels.csv')
     datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        horizontal_flip=True,
-        vertical_flip=True
-    )
+        validation_split=0.25,
+        rescale=1. / 255)
 
-    labels = pd.read_csv(f'{path}/labels.csv')
-    train_datagen = datagen.flow_from_dataframe(
-        labels,
-        path,
+    test_gen_flow = datagen.flow_from_dataframe(
+        dataframe=labels,
+        directory=path + 'final_files/',
+        x_col='file_name',
+        y_col='real_age',
+        target_size=(224, 224),
         batch_size=16,
-        target_size=(150, 150),
-        subset='training',
-        class_mode='sparse',
+        class_mode='raw',
+        subset='validation',
         seed=12345)
 
-    return train_datagen
+    return test_gen_flow
 
 
 def create_model(input_shape):
+    optimizer = Adam(lr=0.0001)
     backbone = ResNet50(input_shape=input_shape,
                         weights='imagenet',
                         include_top=False)
     model = Sequential()
     model.add(backbone)
     model.add(GlobalAveragePooling2D())
-    model.add(Dense(units=100,  activation='softmax'))
-    model.compile(optimizer='adam', loss=tf.keras.losses.MeanAbsoluteError(),
+    model.add(Dense(units=1,  activation='relu'))
+    model.compile(optimizer=optimizer, loss=tf.keras.losses.MeanAbsoluteError(),
                   metrics=['mae'])
     return model
 
 
-def train_model(model, train_data, test_data, epochs=9, batch_size=16,
+def train_model(model, train_data, test_data, batch_size=None, epochs=20,
                 steps_per_epoch=None, validation_steps=None):
+
     model.fit(train_data,
               validation_data=test_data,
-              epochs=epochs,
+              batch_size=batch_size, epochs=epochs,
               steps_per_epoch=steps_per_epoch,
               validation_steps=validation_steps,
-              verbose=2, shuffle=True,
-              loss=tf.keras.losses.MeanAbsoluteError())
+              verbose=2)
 
     return model
